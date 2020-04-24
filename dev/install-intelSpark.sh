1fb4f953e0c78f837b443ad80867359e53e08698
zhixingheyi-tian <1564481943@qq.com>
2020-04-25 01:05:12 +0800

    add nativesql Spark

--- dev/install-intelSpark.sh
+++ dev/install-intelSpark.sh
@@ -2,7 +2,7 @@
 set -ex
 
 cd /tmp
-git clone -b v2.4.4 https://github.com/Intel-bigdata/spark.git
+git clone -b native-sql-engine-clean https://github.com/Intel-bigdata/spark.git
 cd spark
 ./build/mvn -q -Pyarn -Phadoop-3.2 -Dhadoop.version=3.2.0 -Dcheckstyle.skip=true -Dmaven.test.skip=true clean install
 echo $(ls -R ~/.m2/repository/org/apache/spark/)
