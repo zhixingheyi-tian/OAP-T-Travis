dcc7f2d5990e089c97c700e51fc40e5d69051e03
zhixingheyi-tian <1564481943@qq.com>
2020-04-25 17:05:00 +0800

    remove quiet

--- dev/install-intelSpark.sh
+++ dev/install-intelSpark.sh
@@ -4,7 +4,7 @@ set -ex
 cd /tmp
 git clone -b native-sql-engine-clean https://github.com/Intel-bigdata/spark.git
 cd spark
-./build/mvn -q -Pyarn -Phadoop-3.2 -Dhadoop.version=3.2.0 -Dcheckstyle.skip=true -Dmaven.test.skip=true clean install
+./build/mvn  -Pyarn -Phadoop-3.2 -Dhadoop.version=3.2.0 -Dcheckstyle.skip=true -Dmaven.test.skip=true clean install
 echo $(ls -R ~/.m2/repository/org/apache/spark/)
 cd ${TRAVIS_BUILD_DIR}
 
